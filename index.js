ec645f1e7231ef7cc9741943a9beca5549d6db36
Julian Olbrich <julianolbrich@gmail.com>
2021-03-25 08:43:02 +0100

    ...!

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function randomnumber() {
   // gibt beim Aufruf 6 unterschiedliche Zufallszahlen in einem Array aus
-  console.log(Array.from({length: 6}, () => Math.floor(Math.random() * 6)));
+  var arr = [];
+  while(arr.length < 6){
+      var r = Math.floor(Math.random() * 100) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  console.log(arr);
 };
 
 // module.exports = randomnumber

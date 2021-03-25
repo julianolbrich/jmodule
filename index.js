adf3e1274b55c95c9438330866c2a166a6ecf76e
Julian Olbrich <julianolbrich@gmail.com>
2021-03-25 08:39:15 +0100

    ....

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const jmodule = require('@thegoodygoody/randomnumbermodule');
-
 function randomnumber() {
   // gibt beim Aufruf 6 unterschiedliche Zufallszahlen in einem Array aus
   console.log(Array.from({length: 6}, () => Math.floor(Math.random() * 6)));
 };
 
-module.exports = randomnumber
+// module.exports = randomnumber
+exports.jmodule = randomnumber()
